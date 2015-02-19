885864130d0a86aa21d1904997d428cf2286fd56
Jeremy Battle <battlejj@gmail.com>
2015-02-19 17:02:11 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var fs = require('fs')
+  , p = require('path')
+  ;
+
+// how to know when you are done?
+function recursiveReaddirSync(path) {
+  var list = []
+    , files = fs.readdirSync(path)
+    , stats
+    ;
+
+  files.forEach(function (file) {
+    stats = fs.lstatSync(p.join(path, file));
+    if(stats.isDirectory()) {
+      list = list.concat(readdirSync(p.join(path, file)));
+    } else {
+      list.push(p.join(path, file));
+    }
+  });
+
+  return list;
+}
+
+module.exports = recursiveReaddirSync;
